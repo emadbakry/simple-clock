f337477797bba5074569a1a2dd8617ef97c27701
emadbakry <101658737+emadbakry@users.noreply.github.com>
2022-12-23 15:10:37 +0300

    clockl_final

--- index.js
+++ index.js
@@ -2,28 +2,32 @@ let second = document.querySelector('.second');
 let minute = document.querySelector('.minute');
 let hour = document.querySelector('.hour');
 
+let updateTime = () => {
+       let dateNow = new Date();
+       let time_in_hours = dateNow.getHours();
+       let time_in_minutes = dateNow.getMinutes();
+       let time_in_seconds = dateNow.getSeconds();
+       second.setAttribute(
+         "style",
+         `transform:translate(0%,-50%) rotate(${time_in_seconds * 6 - 90}deg);`
+       );
+       minute.setAttribute(
+         "style",
+         `transform:translate(0%,-50%) rotate(${time_in_minutes * 6 - 90}deg);`
+       );
+       hour.setAttribute(
+         "style",
+         `transform:translate(0%,-50%) rotate(${
+           time_in_hours * 30 - 90 + time_in_minutes / 2
+         }deg);`
+       );
+}
+updateTime();
+
 setInterval(() => {
-    
-    let dateNow = new Date();
-    
-    let time_in_hours = dateNow.getHours();
-    let time_in_minutes = dateNow.getMinutes();
-    let time_in_seconds = dateNow.getSeconds();
-
-    // console.log(`${time_in_seconds} ${time_in_minutes} ${time_in_hours}`);
-
-    second.setAttribute(
-      "style",
-      `transform:translate(0%,-50%) rotate(${(time_in_seconds * 6) - 90}deg);`
-    );
-    minute.setAttribute(
-      "style",
-      `transform:translate(0%,-50%) rotate(${(time_in_minutes * 6) - 90}deg);`
-    );
-    hour.setAttribute(
-      "style",
-      `transform:translate(0%,-50%) rotate(${(time_in_hours * 30) - 90 + (time_in_minutes / 2)}deg);`
-    );
+    updateTime();
 }, 1000);
+document.querySelector(".clock").style.opacity = '1';
+
 
 // console.log(dateNow.getHours())
